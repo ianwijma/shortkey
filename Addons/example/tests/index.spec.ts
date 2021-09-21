2ccf44b6583c09db33ea104ed344d61e531fd355
Ian Wijma <ian@wij.ma>
2021-09-21 13:28:09 +1000

    Test cleanup
    I need to write some example tests.
    I also probably need to write some helpers which help with testing the addons.

--- Addons/example/tests/index.spec.ts
+++ Addons/example/tests/index.spec.ts
@@ -1,30 +1,5 @@
-import ImageEditor from '@src/imageEditor'
-import path from 'path'
-
-describe('Edits images', () => {
-  it('Parses and save image', async () => {
-    const editor = new ImageEditor({ w: 1920, h: 1080 })
-    const layer = editor.ensureLayer()
-    layer.setImage({
-      url: path.join(__dirname, 'assets', 'jawoodle.jpeg'),
-      posX: 0,
-      posY: 0,
-      imgWidth: 500,
-    })
-
-    layer.setText({
-      text: 'Jawoodle',
-      posX: 500,
-      posY: 0,
-      font: '150px sans-serif',
-      baseline: 'hanging',
-      align: 'start',
-    })
-
-    const render = await editor.render()
-    await render.save({
-      path: path.join(__dirname, 'out', 'jawoodle.png'),
-      format: 'png',
-    })
+describe('Addon works', () => {
+  it('Works', async () => {
+    // TODO: Write example tests here, probably need to write test helpers in the addon extension
   })
 })
